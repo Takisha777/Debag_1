e4bf43a2976b9304697e75ba62f5af1c9c09ca89
Takisha777 <alexey.kovtun.od@gmail.com>
2024-05-22 14:13:17 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// Source
+
+/*let health = prompt('Введите число параметра "здоровье" для персонажа')
+if () {
+    health < 0 || health!
+        alert('Параметр "здоровье" должен быть больше нуля!')
+} else () {
+    alert(Параметр "здоровье" равен ${Health});
+}*/
+
+let health = Number(prompt('Введите число параметра "здоровье" для персонажа'));
+console.log(health);
+debugger;
+if (health <= 0) {
+    debugger;
+    alert('Параметр "здоровье" должен быть больше нуля!')
+
+} else  {
+    debugger;
+    alert(`Параметр "здоровье" равен ${health}`);
+}
